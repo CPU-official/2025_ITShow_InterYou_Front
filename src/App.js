c5d043336f5dbbf1f5681b26c39e0722029e51ed
ãjdu026 <s2434@e-mirim.hs.kr>
2025-06-06 01:11:13 +0900

    feat: [jung] MainPage

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Main_Page from "./MainPage/Main_Page"; // Main_Page 컴포넌트 가져오기
+
+function App() {
+  return <Main_Page />; // Main_Page 컴포넌트를 렌더링
+}
+
+export default App;
